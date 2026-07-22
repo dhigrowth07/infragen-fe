d4503a24362e9fc4491c2d4c92783a5c42d2e181
jayaraj <madhesjayaraj@gmail.com>
2026-07-23 00:17:39 +0530

    initial setup

---
+++ infragen/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="hero-heading">Welcome to the Homepage</h1>
+    </main>
+  );
+}
---
+++ infragen/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () =>{
+    return(
+        <footer>
+            <p>© 2026 Infragen. All rights reserved.</p>
+        </footer>
+
+    )
+}
